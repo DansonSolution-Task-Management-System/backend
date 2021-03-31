bb08612b29e4600b8272e3d975b337c3c13fb50f
Abishek Khanal <45481516+abishek12@users.noreply.github.com>
2021-03-31 13:30:58 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<?php require_once "include/header.php"; ?>
 
+<body>
+  <div class="grid-container">
+    <!-- header starts -->
+    <?php require_once "include/navigation.php"; ?>
+
+    <!-- sidenav starts -->
+    <?php require_once "include/sidebar.php"; ?>
+
+    <!-- main starts -->
+    <main class="main">
+      <!-- content goes here -->
+
+      <!-- content goes here -->
+    </main>
+
+    <?php
+    require_once "include/footer.php";
+    ?>
